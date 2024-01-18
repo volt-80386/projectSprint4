d0ae97bcfac98f02186f5e57335df0a189c9aa1d
Vladimir <voltvv@yandex.ru>
2024-01-18 21:28:21 +0300

    added order tests

--- src/main/java/project/sprint4/pageobject/Main.java
+++ src/main/java/project/sprint4/pageobject/Main.java
@@ -39,4 +39,48 @@ public class Main {
     public String checkTextAnswer(int i) {
         return webDriver.findElement(By.xpath(String.format(".//div[@id='accordion__panel-%d']/p", i))).getText();
     }
+
+    // Нажать на Заказать
+    public void clickOnOrder() {
+        webDriver.findElement(By.className("Button_Button__ra12g")).click();
+    }
+
+    // Заполнить поля, кроме Станция метро
+    public void fillData(String name, String surname, String address, String telephone) {
+        webDriver.findElement(By.xpath(String.format(".//input[@placeholder='* Имя']"))).sendKeys(name);
+        webDriver.findElement(By.xpath(String.format(".//input[@placeholder='* Фамилия']"))).sendKeys(surname);
+        webDriver.findElement(By.xpath(String.format(".//input[@placeholder='* Адрес: куда привезти заказ']"))).sendKeys(address);
+        webDriver.findElement(By.xpath(String.format(".//input[@placeholder='* Телефон: на него позвонит курьер']"))).sendKeys(telephone);
+    }
+
+    // Заполнить поле Станция метро
+    public void fillStation(String station) {
+        webDriver.findElement(By.xpath(String.format(".//input[@placeholder='* Станция метро']"))).sendKeys(station);
+        webDriver.findElement(By.className("select-search__select")).click();
+    }
+
+    // Нажать на Далее
+    public void clickOnNext() {
+        webDriver.findElement(By.xpath(String.format(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"))).click();
+    }
+
+    // Выбрать дату доставки
+    public void selectDate(int i) {
+        webDriver.findElement(By.xpath(String.format(".//input[@placeholder='* Когда привезти самокат']"))).click();
+        webDriver.findElement(By.xpath(String.format(".//div[text() = '"+ i +"']"))).click();
+    }
+
+    // Выбрать срок аренды
+    public void selectPeriod(String i) {
+        webDriver.findElement(By.xpath(String.format(".//div[text() = '* Срок аренды']"))).click();
+        webDriver.findElement(By.xpath(String.format(".//div[text() = '"+ i +"']"))).click();
+    }
+
+    // Заказать
+    public void selectConfirm() {
+        webDriver.findElement(By.xpath(String.format(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']"))).click();
+        webDriver.findElement(By.xpath(String.format(".//button[text() = 'Да']"))).click();
+    }
+
+
 }
